9da2e0bac28676de72f0701f77aa3e5b56b6de20
davidnb3 <david.mario.pieri@gmail.com>
2021-09-13 19:34:20 +0200

    created Dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useState } from "react";
+import useAuth from "./useAuth";
+import { Container } from "@chakra-ui/react";
+import AddButton from "./AddButton.js";
+import CreateMatch from "./CreateMatch.js";
+import Grid from "./Grid.js";
 
 export default function Dashboard({ code }) {
-    return (
-        <div>
-            {code}
-        </div>
-    )
+  const accessToken = useAuth(code);
+  const [createMatch, setCreateMatch] = useState(false);
+
+  return (
+    <Container p="0" m="0" maxW="none" bgColor="#2D3748" h="100vh">
+      <Container maxW="sm">
+        <AddButton setCreateMatch={setCreateMatch} createMatch={createMatch} />
+        {createMatch ? <CreateMatch /> : <Grid />}
+      </Container>
+    </Container>
+  );
 }
