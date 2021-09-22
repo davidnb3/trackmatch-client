b04976235010c0da332fb420fac2f8e7ff577627
davidnb3 <david.mario.pieri@gmail.com>
2021-09-22 19:14:59 +0200

    form input + api call

--- src/CreateMatch.js
+++ src/CreateMatch.js
@@ -1,7 +1,40 @@
-import React from "react";
-import { Container } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { Container, Input, FormControl } from "@chakra-ui/react";
+import SpotifyWebApi from "spotify-web-api-node";
+import axios from "axios";
 
-export default function CreateMatch() {
+const spotifyApi = new SpotifyWebApi({
+  clientId: "cda05a1cfab241769542ed054c843d20",
+});
+
+export default function CreateMatch({ accessToken }) {
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  //___SET NEW ACCESSTOKEN ON API WHEN ACCESSTOKEN CHANGES
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+
+    axios
+      .get(`https://api.spotify.com/v1/search?q=human%20error&type=track`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [search, accessToken]);
+
   return (
     <Container
       data-testid="createMatch"
@@ -9,7 +42,17 @@ export default function CreateMatch() {
       p={6}
       display="flex"
       justifyContent="right"
-      bgColor="blue"
-    ></Container>
+      bgColor="white"
+      h="calc(100vh - 88px)"
+    >
+      <FormControl id="searchSpotify" isRequired>
+        <Input
+          type="search"
+          placeholder="Search Spotify"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </FormControl>
+    </Container>
   );
 }
