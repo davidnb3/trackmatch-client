9dfc9f3465a9f776b4cef375f199bb5fcc1b65e6
davidnb3 <david.mario.pieri@gmail.com>
2021-09-22 19:16:16 +0200

    completed auth hook

--- src/useAuth.js
+++ src/useAuth.js
@@ -1,16 +1,51 @@
-import {useState, useEffect} from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function useAuth(code) {
-    const [accessToken, setAccessToken] = useState();
-    const [refreshToken, setRefreshToken] = useState();
-    const [expiresIn, setExpiresIn] = useState();
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+  const [expiresIn, setExpiresIn] = useState();
 
-    useEffect(() => {
-        axios.post('http://localhost:3001/login', {
-            code,
-        }).then(res => {
-            console.log(res.data);
+  //___SENDS AUTH CODE TO API TO GET BACK ACCESSTOKEN & REFRESHTOKEN
+  useEffect(() => {
+    axios
+      .post("http://localhost:3001/login", {
+        // AUTH code which is extracted from URL params
+        code,
+      })
+      .then((res) => {
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        // Remove URL parameter
+        window.history.pushState({}, null, "/");
+      })
+      .catch(() => {
+        window.location = "/";
+      });
+  }, [code]);
+
+  //__AUTOMATICALLY SENDS REFRESHTOKEN TO API TO GET BACK A NEW ACCESSTOKEN
+  useEffect(() => {
+    // Avoid API call before there's a refreshToken and expiresIn
+    if (!refreshToken || !expiresIn) return;
+    // Interval to always call API one minute before accessToken expires
+    const interval = setInterval(() => {
+      axios
+        .post("http://localhost:3001/refresh", {
+          refreshToken,
         })
-    }, [code])
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          setExpiresIn(res.data.expiresIn);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, (expiresIn - 60) * 1000);
+    return () => clearInterval(interval);
+    // Sending refresh code to the server
+  }, [refreshToken, expiresIn]);
+
+  return accessToken;
 }
