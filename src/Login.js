69505cb03ca9f5acf8a4e081079215d9b99fec7f
davidnb3 <david.mario.pieri@gmail.com>
2021-09-27 16:15:13 +0200

    replaced clientID inside auth url

--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,21 @@
-import React from 'react'
-import { Box, Center, Container } from "@chakra-ui/react"
-import { Link } from "@chakra-ui/react"
+import React from "react";
+import { Container } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/react";
 
-const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=cda05a1cfab241769542ed054c843d20&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=a20026b3038c4dac8c664ace9f0f4c8e&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
-    return (
-        <Container
-            minH='100vh'
-            centerContent
-            display='flex'
-            justifyContent='center'>
-            <Link
-                href={AUTH_URL}
-                bg='green'
-                color='white'
-                p={2}
-                borderRadius='8px'>
-                    Login with Spotify
-            </Link>
-        </Container>
-    )
+  return (
+    <Container
+      minH="100vh"
+      centerContent
+      display="flex"
+      justifyContent="center"
+    >
+      <Link href={AUTH_URL} bg="green" color="white" p={2} borderRadius="8px">
+        Login with Spotify
+      </Link>
+    </Container>
+  );
 }
