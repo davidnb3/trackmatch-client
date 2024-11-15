92ff4f38fc26c8d704925f2e0319ea1eefb6c271
david-mario.pieri <david-mario.pieri@ionos.com>
2024-11-15 23:23:07 +0100

    remove spotify player script

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <script src="https://sdk.scdn.co/spotify-player.js"></script>
         <App />
       </ThemeProvider>
     </Provider>
